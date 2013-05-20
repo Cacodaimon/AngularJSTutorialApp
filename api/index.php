9eec84db32892931e91e262c2b6c891fef89eaf9
Guido Kroemer <mail@cacodaemon.de>
2013-05-20 19:11:10 +0200

    Added favicon handling

--- api/index.php
+++ api/index.php
@@ -10,7 +10,23 @@ $db = new PDO('sqlite:db.sqlite3');
 function getTitleFromUrl($url)
 {
     preg_match('/<title>(.+)<\/title>/', file_get_contents($url), $matches);
+
     return mb_convert_encoding($matches[1], 'UTF-8', 'UTF-8');
+}
+
+function getFaviconFromUrl($url)
+{
+    $url = parse_url($url);
+    $url = urlencode(sprintf('%s://%s', 
+        isset($url['scheme']) ? $url['scheme'] : 'http', 
+        isset($url['host']) ? $url['host'] : strtolower($url['path'])));
+    
+    return "http://g.etfv.co/$url";
+}
+
+function saveFavicon($url, $id)
+{
+    file_put_contents("../icons/$id.ico", file_get_contents(getFaviconFromUrl($url)));
 }
 
 function returnResult($action, $success = true, $id = 0)
@@ -40,24 +56,28 @@ $app->post('/bookmark', function () use ($db, $app) {
         $url = $app->request()->post('url'),
         empty($title) ? getTitleFromUrl($url) : $title,
     ]);
+    saveFavicon($url, $id = $db->lastInsertId());
 
-    returnResult('add', $sth->rowCount() == 1, $db->lastInsertId());
+    returnResult('add', $sth->rowCount() == 1, $id);
 });
 
 $app->put('/bookmark/:id', function ($id) use ($db, $app) {
     $sth = $db->prepare('UPDATE bookmark SET title = ?, url = ? WHERE id = ?;');
     $sth->execute([
         $app->request()->post('title'),
-        $app->request()->post('url'),
+        $url = $app->request()->post('url'),
         intval($id),
     ]);
+    saveFavicon($url, $id);
 
-    returnResult('edit', $sth->rowCount() == 1, $id);
+    returnResult('add', $sth->rowCount() == 1, $id);
 });
 
 $app->delete('/bookmark/:id', function ($id) use ($db) {
     $sth = $db->prepare('DELETE FROM bookmark WHERE id = ?;');
     $sth->execute([intval($id)]);
+
+    unlink("../icons/$id.ico");
 
     returnResult('delete', $sth->rowCount() == 1, $id);
 });
